aebb7fd8a2eb6f3f91d391f62b50ca6b84c7991c
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-06-22 18:48:47 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,11 +1,17 @@
 function solution(a, b, c) {
+    var setSize = new Set([a, b, c]).size
+    
     var answer = 0;
-    if (a != b && b != c && a != c) {
-        answer = a + b + c
-    } else if (a == b && b == c && a == c) {
-        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
-    } else {
-        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+    
+    switch (setSize) {
+        case 1 : answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+            break;
+        case 2: 
+            answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+            break
+        case 3 :
+            answer = a + b + c
+            break
     }
     return answer;
     
