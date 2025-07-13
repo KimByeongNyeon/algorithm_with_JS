92f657e75bf405a5880bbd0e7b1d0a41f6d7f050
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-07-13 21:04:41 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,11 +1,9 @@
 function solution(arr, idx) {
-    var answer = 0;
+    var answer = -1;
     for (var i = idx; i < arr.length; i++) {
         if (arr[i] == 1) {
             answer = i
             return answer
-        } else if (arr[i] != 1) {
-            answer = -1
         }
     }
     return answer;
