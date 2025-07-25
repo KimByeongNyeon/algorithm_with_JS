945376fc862f8cf0b4b7c7a2fd3ed557ca42c1d3
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-07-25 19:41:30 +0900

    [level 1] Title: 자릿수 더하기, Time: 3.01 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,14 @@
+function solution(n)
+{
+    var str = String(n)
+    var answer = 0;
+    
+    for (var i = 0; i < str.length; i++) {
+        answer += parseInt(str[i])
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return answer;
+}
