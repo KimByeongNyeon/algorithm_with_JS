9abe3cfdf7494152f0531cd1a285d29694195996
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-07-27 15:54:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.98 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,20 @@
+function solution(participant, completion) {
+    var answer = '';
+    var people = {}
+    var counts = []
+    
+    participant.forEach((name) => {
+        counts[name] = (counts[name] || 0) + 1;
+    })
+    
+    completion.forEach((name) => {
+        counts[name] -= 1
+    })
+    for (const name in counts) {
+        if (counts[name] > 0) {
+            return name
+        }
+    }
+    
+    return answer;
+}
