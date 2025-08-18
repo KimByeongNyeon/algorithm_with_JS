fbc43facee538fa391c68c2ce93ec60f54c97705
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-08-18 21:05:48 +0900

    [level 2] Title: 기능개발, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,22 @@
+function solution(progresses, speeds) {
+    let answer = [];
+
+    while (progresses.length > 0) {
+        for (let i = 0; i < progresses.length; i++) {
+            progresses[i] += speeds[i];
+        }
+
+        let count = 0;
+        while (progresses.length > 0 && progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            count++;
+        }
+
+        if (count > 0) {
+            answer.push(count);
+        }
+    }
+
+    return answer;
+}
