73708302e0d91fb853e8cf76a6d61b106ed984d6
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-10-13 16:53:24 +0900

    [level 2] Title: 의상, Time: 4.92 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,24 @@
+function solution(clothes) {
+    var answer = 1;
+    const map = new Map()
+    
+    clothes.forEach((value) => {
+        const [name, type] = value
+        
+        if (!map.has(type)) {
+            map.set(type, 1)
+        }
+        else {
+            map.set(type, map.get(type)+1);
+        }
+    })
+    
+    
+    for (const [key, value] of map) {
+        answer *= (value + 1)
+    }
+    
+    console.log(map)
+
+    return answer - 1;
+}
