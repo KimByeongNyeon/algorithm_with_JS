47845cfd9d71d86b5af972de20d8ed773b68075e
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-11-27 22:07:16 +0900

    [Gold V] Title: 노드사이의 거리, Time: 392 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "dev/stdin"
+      : path.join(__dirname, "input.txt")
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const solution = (input) => {
+  let idx = 0;
+  const [N, M] = input[idx++].split(" ").map(Number);
+
+  const graph = Array.from({ length: N + 1 }, () => []);
+
+  for (let i = 0; i < N - 1; i++) {
+    const [a, b, c] = input[idx++].split(" ").map(Number);
+    graph[a].push({ to: b, cost: c });
+    graph[b].push({ to: a, cost: c });
+  }
+  let answer = [];
+  for (let i = 0; i < M; i++) {
+    const [start, end] = input[idx++].split(" ").map(Number);
+
+    const bfs = () => {
+      const visited = Array.from({ length: N }, () => false);
+      let queue = [[start, 0]];
+      visited[start] = true;
+
+      let head = 0;
+      while (queue.length > head) {
+        const [cur, dist] = queue[head++];
+
+        if (cur === end) return dist;
+
+        for (const { to, cost } of graph[cur]) {
+          if (!visited[to]) {
+            visited[to] = true;
+            queue.push([to, dist + cost]);
+          }
+        }
+      }
+      return 0;
+    };
+
+    console.log(bfs());
+  }
+};
+solution(input);
