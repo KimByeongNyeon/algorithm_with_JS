ef40a976fc155d525ccb341d6dab80a5925a8fe2
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2025-12-15 21:44:04 +0900

    [Bronze II] Title: 방 배정하기, Time: 180 ms, Memory: 9652 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.js
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.js
@@ -13,10 +13,10 @@ const input = fs
 const solution = (input) => {
   const [a, b, c, N] = input[0].split(" ").map(Number);
   let result = 0;
-  for (let i = 0; i < a; i++) {
-    for (let j = 0; j < b; j++) {
-      for (let k = 0; k < c; k++) {
-        if (i * a + j * b + (k + c) === N) {
+  for (let i = 0; i < 51; i++) {
+    for (let j = 0; j < 51; j++) {
+      for (let k = 0; k < 51; k++) {
+        if (i * a + j * b + k * c === N) {
           result = 1;
         }
       }
