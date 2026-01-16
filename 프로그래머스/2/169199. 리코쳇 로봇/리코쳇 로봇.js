51185ec6a3899ad7b2d63d8527058a01e6e2bc65
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2026-01-16 13:45:06 +0900

    [level 2] Title: 리코쳇 로봇, Time: 14.90 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,56 +1,90 @@
 const solution = (board) => {
-  var answer = 0;
-  const queue = [];
   const n = board.length;
   const m = board[0].length;
   board = board.map((row) => row.split(""));
 
+  let answer = 0;
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
+
+  const visited = Array.from({ length: n }, () => Array(m).fill(false));
+
+  const queue = [];
+
+  const gCoord = [];
+
+  // 시작 지점 찾기
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-      if (board[i][j] === "R") queue.push([i, j]);
+      if (board[i][j] === "R") {
+        queue.push([i, j, 0]); // dist = 0
+        visited[i][j] = true;
+      }
+      if (board[i][j] === "G") {
+        gCoord.push(i, j);
+      }
     }
   }
 
-  const visited = Array.from({ length: n }, () => Array(m).fill(false));
-  visited[queue[0][0]][queue[0][1]] = true;
+  const isPossible = (g) => {
+    const [x, y] = g;
 
+    for (let k = 0; k < 4; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) return true;
+
+      if (board[nx][ny] === "D") return true;
+    }
+    return false;
+  };
+
+  const tiket = isPossible(gCoord);
   const bfs = () => {
     let head = 0;
+
     while (queue.length > head) {
-      const size = queue.length - head;
-      for (let s = 0; s < size; s++) {
-        const [x, y] = queue[head++];
+      const [x, y, dist] = queue[head++];
 
-        for (let k = 0; k < 4; k++) {
-          let nx = x 
-          let ny = y
+      for (let k = 0; k < 4; k++) {
+        let nx = x;
+        let ny = y;
 
-          while (true) {
-            const tx = nx + dx[k];
-            const ty = ny + dy[k];
+        // 한 방향으로 끝까지 이동
+        while (
+          nx + dx[k] >= 0 &&
+          nx + dx[k] < n &&
+          ny + dy[k] >= 0 &&
+          ny + dy[k] < m &&
+          board[nx + dx[k]][ny + dy[k]] !== "D"
+        ) {
+          nx += dx[k];
+          ny += dy[k];
+        }
 
-            if (tx < 0 || tx >= n || ty < 0 || ty >= m) break;
-            if (board[tx][ty] === "D") break;
+        // 한 칸도 이동하지 못한 경우는 무시
+        if (nx === x && ny === y) continue;
 
-            nx = tx;
-            ny = ty;
-          }
+        // 목표 지점 도달
+        if (board[nx][ny] === "G") return dist + 1;
 
-          if (nx === x && ny === y) continue;
-
-          if (board[nx][ny] === "G") return answer + 1;
-
-          if (!visited[nx][ny]) {
-            visited[nx][ny] = true;
-            queue.push([nx, ny]);
-          }
+        if (!visited[nx][ny]) {
+          // 방문 처리
+          visited[nx][ny] = true;
+          // dist 추가 및 다음 좌표 탐색
+          queue.push([nx, ny, dist + 1]);
         }
       }
-      answer++;
     }
+
     return -1;
   };
-  return bfs();
+
+  if (!tiket) {
+    return -1;
+  } else {
+    answer = bfs();
+    return answer;
+  }
 };
