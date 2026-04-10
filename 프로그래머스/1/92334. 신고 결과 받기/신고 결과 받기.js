ef7dbf0419019fac4304f8d33f7feb83b2635d6d
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2026-04-11 02:08:26 +0900

    [level 1] Title: 신고 결과 받기, Time: 278.66 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,26 @@
+function solution(id_list, report, k) {
+    var temp = [...new Set(report)]
+    var answer = [];
+    const obj = {}
+    const name = {}
+    id_list.forEach((value) => {
+        name[value] = []
+    })
+    
+    temp.forEach((r) => {
+        const [from, to] = r.split(" ");
+        obj[to] = (obj[to] || 0) + 1;
+    });
+    
+    temp.forEach((r) => {
+        const [from, to] = r.split(" ")
+        if (obj[to] >= k) {
+            name[from].push(to)
+        }
+    })
+    const keys = Object.keys(name)
+    for (let i = 0; i < keys.length; i++) {
+        answer.push(name[keys[i]].length)
+    }
+    return answer;
+}
