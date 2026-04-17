fe8707038444c09c1a38c5b20577fc42ad4a2cf4
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2026-04-17 17:51:10 +0900

    [D2] Title: [S/W 문제해결 기본] 2일차 - 이진탐색, Time: 55 ms, Memory: 51,328 KB -BaekjoonHub

---
+++ SWEA/D2/4839. ［S／W 문제해결 기본］ 2일차 － 이진탐색/［S／W 문제해결 기본］ 2일차 － 이진탐색.py
@@ -0,0 +1,25 @@
+import math
+
+T = int(input())
+for tc in range(T) :
+    p, a, b = map(int, input().split(" "))
+    def binSearch(l,r,end) :
+        cnt = 0
+        while l <= r :
+            mid = math.floor((l + r) / 2)
+            
+            cnt += 1
+            
+            if mid == end:
+                return cnt
+            if mid < end:
+                l = mid
+            elif mid > end:
+                r = mid 
+            
+    answerA = binSearch(1, p, a)
+    answerB = binSearch(1, p, b)
+    if answerA < answerB : print(f"#{tc + 1 } A")
+    elif answerB < answerA : print(f'#{tc + 1 } B')
+    else : print(f'#{tc + 1 } 0')
+    
