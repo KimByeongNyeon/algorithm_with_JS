4f27746f7463dc1ab2e588514bd325ce5a8fe771
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2026-06-08 21:00:27 +0900

    [level 2] Title: 모음 사전, Time: 14.61 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,27 +1,30 @@
-function solution(word) {
-    var answer = 0;
-    // 알파벳을 담은 배열
-    var alpha = ["A", "E", "I", "O", "U"]
-    // flag 값을 통한 제어
-    var flag = false;
-    const dfs = (char) => {
-        // 단어의 길이가 5 이상일 때 혹은 flag 가 true 일 경우 함수 종료
-        if (char.length > 5 || flag) return;
-        
-        // 만약 해당 단어를 찾으면
-        if (char == word) {
-            // flag true 로 변경
-            flag = true;
-            // 함수 종료
-            return;
-        }
-        // answer 누적
-        answer += 1
-        
-        for (var i = 0; i < alpha.length; i++) {
-            dfs(char + alpha[i])
-        }
+const solution = (word) => {
+  let answer = 0;
+  let map = {
+    1: "A",
+    2: "E",
+    3: "I",
+    4: "O",
+    5: "U",
+  };
+  let set = [];
+  // dfs 사용
+  const dfs = (str) => {
+    if (str.length > 5) return;
+    set.push(str);
+
+    for (let i = 0; i <= 5; i++) {
+      dfs(str + map[i]);
     }
-    dfs('')
-    return answer;
-}
+  };
+  for (let i = 1; i <= 5; i++) {
+    dfs(map[i]);
+  }
+  console.log(set);
+  set.sort();
+  for (let i = 0; i < set.length; i++) {
+    if (word === set[i]) answer = i + 1;
+  }
+  console.log(set);
+  return answer;
+};
