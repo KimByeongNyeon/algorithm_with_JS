ff0baa7111f92af61024e3602e7e79faf45cfe0c
KimByeongNyeon <104885355+KimByeongNyeon@users.noreply.github.com>
2026-06-16 00:47:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,17 +1,21 @@
-function solution(num) {
-    var answer = 0;
-    
-    while (num !== 1) {
-        if (answer === 500) {
-            return -1;
-        }
-            
-        if (num % 2 == 0) {
-            num = num / 2
-        }else {
-           num = num * 3 + 1
-        } 
-        answer++
+const solution = (num) => {
+  const recur = (num, depth) => {
+    if (num === 1) {
+      return depth;
     }
-    return answer;
-}
+    if (depth > 500) {
+      return -1;
+    }
+
+    if (num % 2 === 0) {
+      let next = num / 2;
+      return recur(next, depth + 1);
+    } else {
+      let next = num * 3 + 1;
+      return recur(next, depth + 1);
+    }
+  };
+  let result = recur(num, 0);
+  return result
+};
+
